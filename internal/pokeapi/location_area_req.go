1e425da44b16406ca54f22431b279c8d398f1577
ross-brown <rbrown_98@yahoo.com>
2024-04-25 15:47:04 -0500

    add explore cmd

--- internal/pokeapi/location_area_req.go
+++ internal/pokeapi/location_area_req.go
@@ -58,3 +58,51 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 
 	return locationAreasResp, nil
 }
+
+func (c *Client) GetLocationArea(locationAreaName string) (LocationArea, error) {
+	url := baseURL + "/location-area/" + locationAreaName
+
+	//check the cache
+	dat, ok := c.cache.Get(url)
+	if ok {
+		fmt.Println("CACHE HIT!")
+		locationArea := LocationArea{}
+		err := json.Unmarshal(dat, &locationArea)
+		if err != nil {
+			return LocationArea{}, err
+		}
+
+		return locationArea, nil
+	}
+	fmt.Println("Cache MISSED")
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationArea{}, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode > 399 {
+		return LocationArea{}, fmt.Errorf("bad status code: %v", res.StatusCode)
+	}
+
+	dat, err = io.ReadAll(res.Body)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	locationArea := LocationArea{}
+	err = json.Unmarshal(dat, &locationArea)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	c.cache.Add(url, dat)
+
+	return locationArea, nil
+}
